8950d5106e9aa383d1a13529a6af3da841dca24d
Baejjjjjjjj <rew1212@khu.ac.kr>
2023-11-27 23:19:37 +0900

    :sparkles: feature: 3- response config

--- src/main/java/com/example/howIsIt/base/BaseResponse.java
+++ src/main/java/com/example/howIsIt/base/BaseResponse.java
@@ -1,4 +1,53 @@
 package com.example.howIsIt.base;
 
-public class BaseResponse {
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.example.howIsIt.base.Code.SUCCESS;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    @JsonProperty("message")
+    private final String message;
+    @JsonProperty("code")
+    private final int code;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    // 요청에 성공한 경우
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // 요청에 실패한 경우
+    public BaseResponse(Code status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+
+
+    public BaseResponse(Boolean isSuccess, String message, int status) {
+        this.isSuccess = isSuccess;
+        this.message = message;
+        this.code = status;
+    }
+
+
+    public BaseResponse(Code status, String message) {
+        this.isSuccess = status.isSuccess();
+        this.message = message;
+        this.code = status.getCode();
+    }
 }
