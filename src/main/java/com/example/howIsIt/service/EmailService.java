3b8689bdece1ee1dcf3ad6bb84bbe65fe804d9b1
realcommang <katlelo@naver.com>
2024-01-04 23:13:01 +0900

    Update EmailService.java

--- src/main/java/com/example/howIsIt/service/EmailService.java
+++ src/main/java/com/example/howIsIt/service/EmailService.java
@@ -23,7 +23,7 @@ public class EmailService {
         message.setFrom("itimeback@gmail.com");
         message.setTo(to);
         message.setSubject(code);
-        message.setText("이메일 인증을 위해 아래의 버튼을 눌러주세요");
+        message.setText("인증코드입니다");
         emailSender.send(message);
 
         return code;
