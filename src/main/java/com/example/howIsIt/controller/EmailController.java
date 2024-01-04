ba949c7781a5444760961071746c743d191c8bec
realcommang <katlelo@naver.com>
2024-01-04 23:12:20 +0900

    [ADD] 이메일 인증 기능

--- src/main/java/com/example/howIsIt/controller/EmailController.java
+++ src/main/java/com/example/howIsIt/controller/EmailController.java
@@ -1,12 +1,10 @@
 package com.example.howIsIt.controller;
 
+import com.example.howIsIt.base.BaseResponse;
 import com.example.howIsIt.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/emails")
@@ -15,11 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
     private final EmailService emailService;
+    String code = "";
+    @GetMapping("/send") //인증코드보냄
+    public BaseResponse sendMail(@RequestParam String to) {
+        code = emailService.sendSimpleMessage(to);
+        return new BaseResponse(true, "요청에 성공하였습니다.", 2000);
+    }
 
-    @GetMapping("/send")
-    public String sendMail(@RequestParam String to) {
-        emailService.sendSimpleMessage(to);
-        return "Email sent!";
+    @GetMapping("/receive") //인증코드확인
+    public BaseResponse getMail(@RequestParam String verify) {
+
+        if(verify.equals(code))
+            return new BaseResponse(true, "요청에 성공하였습니다.", 2000);
+        else
+            return new BaseResponse(false, "권한이 없는 유저의 접근입니다.", 4000);
     }
 }
 
--- src/main/java/com/example/howIsIt/service/EmailService.java
+++ src/main/java/com/example/howIsIt/service/EmailService.java
@@ -1,7 +1,9 @@
 package com.example.howIsIt.service;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import java.util.Random;
 
 @Service
 public class EmailService {
@@ -12,13 +14,24 @@ public class EmailService {
         this.emailSender = emailSender;
     }
 
-    public void sendSimpleMessage(String to) {
+    public String sendSimpleMessage(String to) {
+
+        int num = randomNum();
+        String code = String.valueOf(num).toString();
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("itimeback@gmail.com");
         message.setTo(to);
-        message.setSubject("portfolier 인증 메일");
+        message.setSubject(code);
         message.setText("이메일 인증을 위해 아래의 버튼을 눌러주세요");
         emailSender.send(message);
+
+        return code;
+    }
+
+    public int randomNum() {
+        Random rand = new Random();
+        return 1000 + rand.nextInt(9000);
     }
 }
 
